83bd6652503ccc4ad8ef9fe423a58f45082c41b2
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-15 17:57:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,8 @@ class Song
     self.all.detect { |song| song.name == song_name }
   end
 
+  def self.find_or_create_by_name
+    find_by_name(song_name) || create_by_name(song_name)
+  end
+  
 end
