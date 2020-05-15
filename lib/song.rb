fad1439a30cf02868574a606e3391c138a0dbb5e
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-15 17:58:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     self.all.detect { |song| song.name == name }
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || create_by_name(name)
   end
 
